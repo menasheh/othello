d52305867394b608dc677f1ee98361a44f54a280
Menasheh Peromsik <menashehyperomsik@gmail.com>
2018-05-29 09:33:49 -0400

    start othelloweb

---
+++ OthelloWeb/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OthelloModel;
+using OthelloWeb.Models;
+
+namespace OthelloWeb.Controllers
+{
+    public static class Game
+    {
+        public static Othello OthelloGame = new Othello();
+
+        public static char ToChar(this int gp)
+        {
+            switch (gp)
+            {
+                case 0:
+                default:
+                    return ' ';
+                case 1:
+                    return '\u25CF';
+                case 2:
+                    return '\u25CB';
+            }
+        }
+    }
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(Game.OthelloGame);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Move(int row, int col)
+        {
+            if (row < 0 || col < 0 || row > 7 || col > 7)
+            {
+                RedirectToAction("Error");
+            }
+
+            Game.OthelloGame.PlaceToken(row, col);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
